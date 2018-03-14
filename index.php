afaca9f75b4eef0de82e374a00a920475b835de7
unknown <ingitsoro@gmail.com>
2018-03-14 17:00:44 +0000

    creation du premier fichier du projet

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DEBUTER AVEC GIT</title>
+    </head>
+    <body>
+        <h1>Hi</h1>
+    </body>
+</html>
